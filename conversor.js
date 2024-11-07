07dd5bf8de4c36ea050439a8d14521ac01cdfe22
naillim <naillim.domingues@alunoescolassociais.org.br>
2024-11-07 09:21:39 -0300

    adiciona funcao para buscar taxa de cambio da api

---
+++ conversor.js
@@ -0,0 +1,20 @@
+// Função para buscar taxa de câmbio da API
+async function getExchangeRate(daMoeda, paraMoeda) {
+  try {
+      const response = await fetch(`${apiURL}${daMoeda}`);
+      const data = await response.json();
+     
+      if (data.result === "success") {
+          return data.conversion_rates[paraMoeda];
+      } else {
+          throw new Error("Erro ao buscar as taxas de câmbio");
+      }
+  } catch (error) {
+      console.error("Erro:", error);
+      return null;
+  }
+}
+
+
+document.getElementById('currency-form').addEventListener('submit', async function(event) {
+  event.preventDefault();
